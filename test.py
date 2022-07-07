e86a4aec15a4cb78f6ef74b8447658d5bcab000f
bhanukanchanam <bhanukanchanam@gmail.com>
2022-07-07 00:33:59 -0400

    Ravish files

--- test.py
+++ test.py
@@ -4,3 +4,4 @@ calc = Calculator(a=20, b=10)
 
 print(f"Addition of the given values: {calc.addition()}")
 print(f"Subtraction of the given values: {calc.subtraction()}")
+print(f"Multiplication of the give values: {calc.multiplication()}")
